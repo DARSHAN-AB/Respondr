d6203151299d1c7e25971143ca3eb8a71948c5ba
DARSHAN A B <darshandarshu20062006@gmail.com>
2024-12-04 00:31:56 +0530

    Initial commit(error present in signup page)

---
+++ app/src/main/java/com/example/respondr/login.java
@@ -0,0 +1,92 @@
+package com.example.respondr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class login extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText loginEmail, loginPassword;
+    private Button loginButton;
+    private TextView signupRedirect;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
+        signupRedirect = findViewById(R.id.signupRedirect);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String loginemail = loginEmail.getText().toString();
+                String loginpassword = loginPassword.getText().toString();
+
+                if(!loginemail.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(loginemail).matches()){
+                    if(!loginpassword.isEmpty()){
+                        auth.signInWithEmailAndPassword(loginemail,loginpassword)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(login.this, MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(login.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        loginPassword.setError("Please enter your password");
+                    }
+                } else if (loginemail.isEmpty()) {
+                    loginEmail.setError("Please enter your email");
+                } else {
+                    loginEmail.setError("Invalid email");
+                }
+
+            }
+        });
+
+        signupRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login.this, signup.class));
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/example/respondr/signup.java
@@ -0,0 +1,96 @@
+package com.example.respondr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class signup extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    EditText signupEmail, signupPassword, signupConfirmPassword;
+    private Button signupButton;
+    private TextView loginRedirect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signup);
+
+        auth = FirebaseAuth.getInstance();
+        signupEmail = findViewById(R.id.signup_email);
+        signupPassword = findViewById(R.id.signup_password);
+        signupConfirmPassword = findViewById(R.id.signup_confirm_password);
+        signupButton = findViewById(R.id.signup_button);
+        loginRedirect = findViewById(R.id.loginRedirect);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String signupemail = signupEmail.getText().toString().trim();
+                String signuppassword = signupPassword.getText().toString().trim();
+                String signupconfirmpassword = signupConfirmPassword.getText().toString().trim();
+
+                if(signupemail.isEmpty()){
+                    signupEmail.setError("Please enter your email");
+                    signupEmail.requestFocus();
+                }
+
+                if(signuppassword != signupconfirmpassword){
+                    signupConfirmPassword.setError("Passwords do not match");
+                    signupConfirmPassword.requestFocus();
+                }
+
+                if(signuppassword.isEmpty()){
+                    signupPassword.setError("Please enter your password");
+                    signupPassword.requestFocus();
+                } else {
+                    auth.createUserWithEmailAndPassword(signupemail, signuppassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+                                Toast.makeText(signup.this, "Signup Successfull", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(signup.this, login.class));
+                            } else {
+                                Toast.makeText(signup.this, "Signup Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        loginRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(signup.this, login.class));
+            }
+        });
+
+    }
+}
